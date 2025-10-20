10d6143096d23b18477fd22b75660a9c1030d04f
AW_RYZEN9 <adam_t_watson@hotmail.com>
2025-10-20 20:02:32 +0100

    clean up prov includes

--- app_wifi_prov.cpp
+++ app_wifi_prov.cpp
@@ -9,28 +9,7 @@
    CONDITIONS OF ANY KIND, either express or implied.
 */
 
-#include <stdio.h>
-#include <string.h>
 
-#include <freertos/FreeRTOS.h>
-#include <freertos/task.h>
-#include <freertos/event_groups.h>
-
-#include <esp_log.h>
-#include <esp_wifi.h>
-#include <esp_event.h>
-#include <nvs_flash.h>
-
-#include <wifi_provisioning/manager.h>
-
-#ifdef CONFIG_EXAMPLE_PROV_TRANSPORT_BLE
-#include <wifi_provisioning/scheme_ble.h>
-#endif /* CONFIG_EXAMPLE_PROV_TRANSPORT_BLE */
-
-#ifdef CONFIG_EXAMPLE_PROV_TRANSPORT_SOFTAP
-#include <wifi_provisioning/scheme_softap.h>
-#endif /* CONFIG_EXAMPLE_PROV_TRANSPORT_SOFTAP */
-#include "qrcode.h"
 
 static const char *TAG = "app";
 int loop_count=0;
--- app_wifi_prov.h
+++ app_wifi_prov.h
@@ -21,8 +21,8 @@
 
 #include <esp_log.h>
 #include <esp_wifi.h>
-#include <esp_event.h>
-//#include <nvs_flash.h>
+//#include <esp_event.h>
+#include <nvs_flash.h>
 
 #include <wifi_provisioning/manager.h>
 
