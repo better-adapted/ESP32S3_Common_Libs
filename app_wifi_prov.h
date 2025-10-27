aeaefcb24d179aaec266178c9322a75dafd1e234
AW_RYZEN9 <adam_t_watson@hotmail.com>
2025-10-27 12:50:09 +0000

    switch to Arduino Prov!

--- app_wifi_prov.h
+++ app_wifi_prov.h
@@ -9,31 +9,10 @@
 */
 
 #include <Arduino.h>
+#include <WiFiProv.h>
+#include <WiFi.h>
 
-#include <esp_chip_info.h>
-
-#include <stdio.h>
-#include <string.h>
-
-#include <freertos/FreeRTOS.h>
-#include <freertos/task.h>
-#include <freertos/event_groups.h>
-
-#include <esp_log.h>
 #include <esp_wifi.h>
-//#include <esp_event.h>
-#include <nvs_flash.h>
-
-#include <wifi_provisioning/manager.h>
-
-#ifdef CONFIG_EXAMPLE_PROV_TRANSPORT_BLE
-#include <wifi_provisioning/scheme_ble.h>
-#endif /* CONFIG_EXAMPLE_PROV_TRANSPORT_BLE */
-
-#ifdef CONFIG_EXAMPLE_PROV_TRANSPORT_SOFTAP
-#include <wifi_provisioning/scheme_softap.h>
-#endif /* CONFIG_EXAMPLE_PROV_TRANSPORT_SOFTAP */
-#include "qrcode.h"
 
 void app_wifi_prov_main(bool reset_settings);
 void wifi_prov_network_reset();
